e95ad97b67d6107d5ae97b09a575b428c074cb34
Lewen Zheng <rockman-zheng@qq.com>
2024-03-13 02:17:18 +0800

    Fix errors
    - Fix portfolio formula
    - Fix cumulative return calculation

--- backtest.py
+++ backtest.py
@@ -11,15 +11,15 @@ def calc_opt_portfolio(model: keras.Model,
     n = outputs.shape[-1]//2
     mu = outputs[:,:n]
     sd = outputs[:,n:]
-    return mu*target_return/np.sum(mu**2/sd**2,axis=-1)
+    return mu/sd**2*target_return/np.sum(mu**2/sd**2,axis=-1).reshape(-1,1)
 
 def calc_return(portfolios: np.ndarray,
                 rates: np.ndarray):
-    return np.cumprod(np.sum(portfolios * rates,axis = -1))
+    return np.cumprod(1+np.sum(portfolios * rates,axis = -1))-1
 
 if __name__ == '__main__':
     model = keras.models.load_model("model.keras")
     _,_,_,_,x_test,y_test = torch.load('data/data.pt')
     portfolios = calc_opt_portfolio(model, x_test)
-    RoR = calc_return(portfolios,y_test)
+    RoR = calc_return(portfolios,np.array(y_test))
     plt.plot(RoR)
