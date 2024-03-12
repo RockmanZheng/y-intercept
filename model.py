497a19f3e2bc9c372e511aa15f0a0f356f0cf842
Lewen Zheng <rockman-zheng@qq.com>
2024-03-12 15:06:10 +0800

    Use reverse order to encode the historical order

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import keras
 from keras import layers
+from keras import ops
 
 class SectorEmbedding(layers.Layer):
     def __init__(self, n_sector, embed_dim):
@@ -27,3 +28,21 @@ class StockEmbedding(layers.Layer):
     
     def call(self, x):
         return self.stock_emb(x)
+
+class PositionalEmbedding(layers.Layer):
+    def __init__(self, maxlen, embed_dim):
+        """
+        Parameters:
+            - maxlen: the maximal length of the look-back period 
+            - embed_dim: the dimension of the embedding vector
+        """
+        super().__init__()
+        self.maxlen = maxlen
+        self.pos_emb = layers.Embedding(input_dim = maxlen,
+                                        output_dim = embed_dim)
+    
+    def call(self, x):
+        maxlen = ops.shape(x)[-1]
+        # use reverse order like 4, 3, 2, 1, 0 to encode the time order
+        positions = ops.arange(start = maxlen-1, stop = -1, step = -1)
+        return self.pos_emb(positions)
