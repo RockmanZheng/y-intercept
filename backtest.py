43bd5f3113da983d92da0f68899bf8a8f4c8e0fe
Lewen Zheng <rockman-zheng@qq.com>
2024-03-13 01:33:39 +0800

    saved model and add backtest script

---
+++ backtest.py
@@ -0,0 +1,25 @@
+import numpy as np
+import keras
+import torch
+import matplotlib.pyplot as plt
+
+
+def calc_opt_portfolio(model: keras.Model,
+                       inputs: np.ndarray,
+                       target_return: float = 0.1):
+    outputs = model.predict(inputs)
+    n = outputs.shape[-1]//2
+    mu = outputs[:,:n]
+    sd = outputs[:,n:]
+    return mu*target_return/np.sum(mu**2/sd**2,axis=-1)
+
+def calc_return(portfolios: np.ndarray,
+                rates: np.ndarray):
+    return np.cumprod(np.sum(portfolios * rates,axis = -1))
+
+if __name__ == '__main__':
+    model = keras.models.load_model("model.keras")
+    _,_,_,_,x_test,y_test = torch.load('data/data.pt')
+    portfolios = calc_opt_portfolio(model, x_test)
+    RoR = calc_return(portfolios,y_test)
+    plt.plot(RoR)
